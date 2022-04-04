1df62f1d73fb996af2eb5611669f432a5dec0a8f
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-04-04 18:12:28 +0400

    update

--- shared/components/Header/Header.styled.tsx
+++ shared/components/Header/Header.styled.tsx
@@ -2,36 +2,38 @@ import { Box } from "@material-ui/core";
 import { Button, Container } from "@mui/material";
 import styled from "styled-components";
 
-
-
-
-export const HeaderStyled:any = styled.header`
-    background: ${({ theme, mode}: any) => mode ? theme.colors.white : theme.colors.blackGrey}} !important;
+export const HeaderStyled: any = styled.header`
+    position:sticky ;
+    top: 0;
+    z-index: 99;
+    background: ${({ theme, mode }: any) =>
+      mode ? theme.colors.white : theme.colors.blackGrey}} !important;
     margin-bottom: 20px;
-`
+`;
 
-export const HeaderContainer:any = styled(Container).attrs(() => ({
-    maxWidth: "lg"
+export const HeaderContainer: any = styled(Container).attrs(() => ({
+  maxWidth: "lg",
 }))`
-    display: flex !important;
-    height: 80px;
-    justify-content: space-between !important;
-    border-bottom:1px solid ${({ theme, mode}: any) => mode ? theme.colors.whiteGray :""} !important;
-    align-items: center !important;
+  display: flex !important;
+  height: 80px;
+  justify-content: space-between !important;
+  border-bottom: 1px solid
+    ${({ theme, mode }: any) => (mode ? theme.colors.whiteGray : "")} !important;
+  align-items: center !important;
+`;
 
-`
-
-export const ModeButton:any = styled(Button).attrs((props:any) => ({
-    variant: "text"
+export const ModeButton: any = styled(Button).attrs((props: any) => ({
+  variant: "text",
 }))`
     min-width: 0 !important;
     margin-left: 5px;
     svg{
-        fill: ${({ theme, mode }: any) => mode ? theme.colors.dark : theme.colors.green}} !important;
+        fill: ${({ theme, mode }: any) =>
+          mode ? theme.colors.dark : theme.colors.green}} !important;
     }
 
-`
+`;
 
 export const MenuActions = styled(Box)`
-/* width: 20%; */
-`
+  /* width: 20%; */
+`;
