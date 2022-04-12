cd451896213889a9fada466c751d9cde922738b6
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-04-12 15:56:23 +0400

    update

--- shared/components/Typograph/Typograph.styled.tsx
+++ shared/components/Typograph/Typograph.styled.tsx
@@ -2,45 +2,51 @@ import { Typography } from "@mui/material";
 import styled, { css } from "styled-components";
 import { ThemeProps, TypographProps } from "../../../types/theme";
 
-
 export const TypographyText = styled(Typography).attrs(() => ({
-    variant: "h1",
-    component: "div"
+  variant: "h1",
+  component: "div",
 }))`
-    padding: 0 !important;
-    ${({ theme, font, color, bold, center, margin,text }:TypographProps<ThemeProps>) => css`
-        color:${() => {
-            switch (color) {
-                case "dark":
-                    return theme.colors.textBlack;
-                case "gray":
-                    return theme.colors.grayText1;
-                case "darkgray":
-                    return theme.colors.grayText2;
-                case "red":
-                    return theme.colors.mainRed;
-                case "white":
-                    return theme.colors.white;
-                case "green":
-                    return theme.colors.green;
-                case "lime":
-                    return theme.colors.green;
-                default:
-                    return theme.colors.textBlack;
-            }
-        }
-        } !important;
-        font-size:${font ? `${font}px` : theme.font.size.medium} !important;
-        margin:${margin ? `${margin}px 2px` : "10px 3px"} !important;
-        font-weight:${bold && `bold`} !important;
-        line-height: 1.5 !important;
-        text-transform:${text ? "lowercase" : "normal"}!important;
-        display: flex;
-        align-items: center;
-        text-align:${center ? `center` : "none"} !important;
-        justify-content:${center ? `center` : "none"} !important;
-        svg{
-            margin-right: 5px;
-        }
-        `}
-`
+  padding: 0 !important;
+  ${({
+    theme,
+    font,
+    color,
+    bold,
+    center,
+    margin,
+    text,
+  }: TypographProps<ThemeProps>) => css`
+    color: ${() => {
+      switch (color) {
+        case "dark":
+          return theme.colors.textDark;
+        case "gray":
+          return theme.colors.whiteGray;
+        case "darkgray":
+          return theme.colors.blackGrey1;
+        case "red":
+          return theme.colors.bgRed;
+        case "white":
+          return theme.colors.white;
+        case "green":
+          return theme.colors.green;
+        case "lime":
+          return theme.colors.green;
+        default:
+          return theme.colors.textDark;
+      }
+    }} !important;
+    font-size: ${font ? `${font}px` : theme.font.size.medium} !important;
+    margin: ${margin ? `${margin}px 2px` : "10px 3px"} !important;
+    font-weight: ${bold && `bold`} !important;
+    line-height: 1.5 !important;
+    text-transform: ${text ? "lowercase" : "normal"}!important;
+    display: flex;
+    align-items: center;
+    text-align: ${center ? `center` : "none"} !important;
+    justify-content: ${center ? `center` : "none"} !important;
+    svg {
+      margin-right: 5px;
+    }
+  `}
+`;
