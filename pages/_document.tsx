7536e113bd0fc7398779b9e849de89d93da2dcde
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2022-07-06 18:40:20 +0400

    connect analytc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -46,29 +46,28 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;900&display=swap" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-BLQJDR1LCY"
+          ></script>
 
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-BLQJDR1LCY"
-        ></script>
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
 
                   gtag('config', 'G-BLQJDR1LCY');
         `,
-          }}
-        />
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     );
   }
