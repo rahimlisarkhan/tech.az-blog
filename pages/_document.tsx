583f6792b06dde94c898ac6e5248c4807af927cc
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2022-07-06 18:33:42 +0400

    connect analytc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ import Document, {
   NextScript,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import Script from "next/script";
 
 export default class MyDocument extends Document {
   static async getInitialProps(
@@ -45,12 +46,29 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;900&display=swap" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" />
-    
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
+
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BLQJDR1LCY"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-BLQJDR1LCY');
+        `,
+          }}
+        />
       </Html>
     );
   }
