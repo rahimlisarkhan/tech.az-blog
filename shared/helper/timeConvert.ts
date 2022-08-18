b711e34d5e965f251d46cc0fc4a16256ded6881f
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2022-08-19 00:07:56 +0400

    fix(news) time convert

--- shared/helper/timeConvert.ts
+++ shared/helper/timeConvert.ts
@@ -1,11 +1,40 @@
+export const convertNormalDate = (
+  givenDate: Date | string | number | undefined
+) => {
+  if (!givenDate) return;
 
+  const date = new Date(givenDate);
 
-export const convertNormalDate = (givenDate: Date | string) => {
-    const date = new Date(givenDate)
-    const clock = (date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()) + ":" + (date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes())
-    const result = (date.getDate() < 10 ? `0${date.getDate()}` : date.getDate())
-        + "." + (date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1)
-        + "." + date.getFullYear()
+  const nowDateDay = new Date().getDate();
+  const nowDateMonth = new Date().getMonth();
+  const nowDateYear = new Date().getFullYear();
+  const nowDateMin = new Date().getMinutes();
+  const nowDateHour = new Date().getHours();
 
-    return `${clock + "  " + result}`
-}
+  const hours = date.getHours();
+  const min = date.getMinutes();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  const clock =
+    (hours < 10 ? `0${hours}` : hours) + ":" + (min < 10 ? `0${min}` : min);
+  const result =
+    (day < 10 ? `0${day}` : day) +
+    "." +
+    (month + 1 < 10 ? `0${month + 1}` : month + 1) +
+    "." +
+    year;
+
+  if (nowDateYear === year && nowDateMonth === month && day === nowDateDay) {
+    return `${
+      nowDateHour - hours !== 0
+        ? `${nowDateHour - hours} saat əvvəl`
+        : nowDateMin - min === 0
+        ? "bu dəqiqə"
+        : nowDateMin - min + " dəqiqə əvvəl"
+    }`;
+  }
+
+  return `${clock + "  " + result}`;
+};
